ce92263e936b133f8d7ece588ac49b71acda567f
k.miyaji <k.miyaji@swallowarc.jp>
2023-05-04 21:03:33 +0900

    add logger

--- internal/core/logger.go
+++ internal/core/logger.go
@@ -1,1 +1,23 @@
 package core
+
+import (
+	"log"
+
+	"go.uber.org/zap"
+)
+
+func NewZapLoggerWithConfig(config zap.Config) *zap.Logger {
+	return build(config)
+}
+
+// build get zap logger instance.
+func build(config zap.Config) *zap.Logger {
+	config.OutputPaths = []string{"stdout"}
+	config.ErrorOutputPaths = []string{"stderr"}
+	logger, err := config.Build()
+	if err != nil {
+		log.Println("create logger failed")
+		log.Fatal(err)
+	}
+	return logger
+}
